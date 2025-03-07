b47eefaff7800f900ec37241e54d2b14accb78d9
nasaska <nas.nas.uwu@gmail.com>
2025-03-07 13:23:45 +0000

    Basic server set up

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 	"log"
+	"net/http"
 )
 
-//all the models requires the getters and setters and basic constructor
+// all the models requires the getters and setters and basic constructor
+type user struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+var users = []user{
+	{Id: 546, Username: "John"},
+	{Id: 894, Username: "Mary"},
+	{Id: 326, Username: "Jane"},
+}
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
 
 func main() {
+
+	router := gin.Default()
+	router.GET("/users", getUsers)
+	router.Run("localhost:8000")
+
 	connStr := "postgres://root:beetroot@localhost:5433/GCT"
 
 	var err error
