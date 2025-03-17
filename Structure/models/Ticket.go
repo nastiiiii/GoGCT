d391aff7c753c7443c6fba5cd7e19a99c8cc8174
nasaska <nas.nas.uwu@gmail.com>
2025-03-17 15:18:29 +0000

    Ticket and Transaction created

--- Structure/models/Ticket.go
+++ Structure/models/Ticket.go
@@ -1,10 +1,5 @@
 package models
 
-import "github.com/jackc/pgx/v5"
-
-// @TODO  figure out enum for tickets
-// @TODO  json back and forth
-
 type BookingStatus string
 
 const (
@@ -13,65 +8,10 @@ const (
 	Payed  BookingStatus = "Payed"
 )
 
-type ITicketService interface {
-	CreateTicket(transactionId int, seat string, performanceId int, ticketStatus BookingStatus) *Ticket
-	GetPrice(performance Performance, performanceSeat string) float64
-	GetPriceByTicket(ticket Ticket) float64
-	GetTicketById(ticketId int) *Ticket
-	GetAllTickets() []Ticket
-	ChangeSeatAvailability(seat string, status BookingStatus)
-	DeleteTicket(ticketId int) bool
-	UpdateTicket(id int, ticket Ticket)
-}
-
 type Ticket struct {
 	TicketId      int
 	TransactionId int
-	Seat          string
 	PerformanceId int
 	TicketStatus  BookingStatus
-}
-
-type TicketService struct {
-	DB *pgx.Conn
-}
-
-func (t *TicketService) CreateTicket(transactionId int, seat string, performanceId int, ticketStatus BookingStatus) *Ticket {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) GetPriceByTicket(ticket Ticket) float64 {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) GetPrice(performance Performance, performanceSeat string) float64 {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) ChangeSeatAvailability(seat string, status BookingStatus) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) GetTicketById(ticketId int) Ticket {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) GetAllTickets() []Ticket {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) DeleteTicket(ticketId int) bool {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t *TicketService) UpdateTicket(id int, ticket Ticket) {
-	//TODO implement me
-	panic("implement me")
+	Seat          string
 }
